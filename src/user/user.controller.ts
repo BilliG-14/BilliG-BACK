27ee434642f84a148a4ff3773a419fc1a65df41f
SonByungjin <sgyos000@gmail.com>
2022-12-15 14:42:55 +0900

    feature: login apply

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dto/createUser.dto';
+import { LoginUserDTO } from './dto/loginUser.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -15,5 +16,11 @@ export class UserController {
   async createUser(@Body() userInfo: CreateUserDTO) {
     const createdUser = await this.userService.create(userInfo);
     return createdUser;
+  }
+
+  @Post('login')
+  async loginUser(@Body() userInfo: LoginUserDTO) {
+    const user = await this.userService.login(userInfo);
+    return user;
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import bcrypt from 'bcrypt';
 
 import { CreateUserDTO } from './dto/createUser.dto';
 import { User, UserDocument } from './schemas/user.schema';
+import { LoginUserDTO } from './dto/loginUser.dto';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  get() {
-    return this.userModel.find({});
+  private async getUserByEmail(email: string) {
+    return await this.userModel.findOne({ email });
   }
 
-  async create(userInfo: CreateUserDTO) {
+  public async get() {
+    const users = await this.userModel.find({});
+    return users;
+  }
+
+  public async create(userInfo: CreateUserDTO) {
     const passwordHashed = await bcrypt.hash(userInfo.password, 10);
     const userInfoModified = {
       ...userInfo,
@@ -22,5 +28,19 @@ export class UserService {
     };
     const createdUser = new this.userModel(userInfoModified);
     return createdUser.save();
+  }
+
+  async login(userInfo: LoginUserDTO) {
+    const user = await this.getUserByEmail(userInfo.email);
+    const isPasswordMatching = await bcrypt.compare(
+      userInfo.password,
+      user.password,
+    );
+
+    if (!isPasswordMatching) {
+      throw new HttpException('잘못된 비밀번호입니다', HttpStatus.BAD_REQUEST);
+    }
+
+    return user;
   }
 }
