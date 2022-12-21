763d02d2916dfddc466411e29ce1de602b55f204
SonByungjin <sgyos000@gmail.com>
2022-12-21 16:06:28 +0900

    feat: suspension check apply

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,6 +1,6 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { TokenPayload } from '../auth.interface';
 import { UserService } from 'src/user/user.service';
 
@@ -16,6 +16,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: TokenPayload) {
     const user = await this.userService.getUserById(payload.id);
+    if (user.suspension) {
+      throw new HttpException('이용 정지된 유저 입니다', HttpStatus.FORBIDDEN);
+    }
     return user;
   }
 }
