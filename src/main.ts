c74439d98b77f74e46a43b3fc22446bc15d330ab
SonByungjin <sgyos000@gmail.com>
2022-12-28 15:53:35 +0900

    Fix: static index html remove

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(
     //class-validation 을 사용하기 위한 등록
@@ -23,7 +21,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  app.useStaticAssets(join(__dirname, '..', 'static'));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
